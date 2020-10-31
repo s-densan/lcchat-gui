b845032da3342aa12103ac125ca518d59ec3d6a7
Yakoi <yakoi.rice@gmail.com>
2020-10-31 17:44:29 +0900

    再始動

--- src/renderer/components/Clock.tsx
+++ src/renderer/components/Clock.tsx
@@ -4,7 +4,7 @@ interface IProps {
   interval: number;
   onTimer: () => void;
 }
-export default const Clock = (props: IProps) => {
+export default function Clock(props: IProps) {
   const [now, setNow] = React.useState(new Date());
   React.useEffect(() => {
     const intervalId = setInterval(() => {
